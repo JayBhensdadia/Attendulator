2b2997248f1e450f93a388442e232170f31911ba
Jay Bhensdadia <jayvpatel29@gmail.com>
2023-07-16 22:56:02 +0530

    Initial Commit

---
+++ Attendulator/AttendulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttendulatorApp.swift
+//  Attendulator
+//
+//  Created by Jay Bhensdadia on 16/07/23.
+//
+
+import SwiftUI
+
+@main
+struct AttendulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
